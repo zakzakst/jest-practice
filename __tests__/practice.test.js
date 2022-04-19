cf4b459f9b6beac926ccb46268ba54224c492d5b
zakzakst <e1433104@gmail.com>
2022-04-19 23:12:54 +0900

    数値

--- __tests__/practice.test.js
+++ __tests__/practice.test.js
@@ -21,20 +21,37 @@
 //   }
 // });
 
-test('null', () => {
-  const n = null;
-  expect(n).toBeNull();
-  expect(n).toBeDefined();
-  expect(n).not.toBeUndefined();
-  expect(n).not.toBeTruthy();
-  expect(n).toBeFalsy();
+// test('null', () => {
+//   const n = null;
+//   expect(n).toBeNull();
+//   expect(n).toBeDefined();
+//   expect(n).not.toBeUndefined();
+//   expect(n).not.toBeTruthy();
+//   expect(n).toBeFalsy();
+// });
+
+// test('zero', () => {
+//   const z = 0;
+//   expect(z).not.toBeNull();
+//   expect(z).toBeDefined();
+//   expect(z).not.toBeUndefined();
+//   expect(z).not.toBeTruthy();
+//   expect(z).toBeFalsy();
+// });
+
+test('two plus two', () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
 });
 
-test('zero', () => {
-  const z = 0;
-  expect(z).not.toBeNull();
-  expect(z).toBeDefined();
-  expect(z).not.toBeUndefined();
-  expect(z).not.toBeTruthy();
-  expect(z).toBeFalsy();
+// 丸め誤差への対処
+test('adding floating point numbers', () => {
+  const value = 0.1 + 0.2;
+  // expect(value).toBe(0.3); // これだとエラー
+  expect(value).toBeCloseTo(0.3);
 });
