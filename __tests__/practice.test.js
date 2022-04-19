ca6461055f110e20422ac17271951afe7c2d3c1f
zakzakst <e1433104@gmail.com>
2022-04-19 23:15:55 +0900

    文字列

--- __tests__/practice.test.js
+++ __tests__/practice.test.js
@@ -39,19 +39,27 @@
 //   expect(z).toBeFalsy();
 // });
 
-test('two plus two', () => {
-  const value = 2 + 2;
-  expect(value).toBeGreaterThan(3);
-  expect(value).toBeGreaterThanOrEqual(3.5);
-  expect(value).toBeLessThan(5);
-  expect(value).toBeLessThanOrEqual(4.5);
-  expect(value).toBe(4);
-  expect(value).toEqual(4);
+// test('two plus two', () => {
+//   const value = 2 + 2;
+//   expect(value).toBeGreaterThan(3);
+//   expect(value).toBeGreaterThanOrEqual(3.5);
+//   expect(value).toBeLessThan(5);
+//   expect(value).toBeLessThanOrEqual(4.5);
+//   expect(value).toBe(4);
+//   expect(value).toEqual(4);
+// });
+
+// // 丸め誤差への対処
+// test('adding floating point numbers', () => {
+//   const value = 0.1 + 0.2;
+//   // expect(value).toBe(0.3); // これだとエラー
+//   expect(value).toBeCloseTo(0.3);
+// });
+
+test('there is no I in team', () => {
+  expect('team').not.toMatch(/I/);
 });
 
-// 丸め誤差への対処
-test('adding floating point numbers', () => {
-  const value = 0.1 + 0.2;
-  // expect(value).toBe(0.3); // これだとエラー
-  expect(value).toBeCloseTo(0.3);
+test('but there is a "stop" in Christoph', () => {
+  expect('Christoph').toMatch(/stop/);
 });
