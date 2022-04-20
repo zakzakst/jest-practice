88c11ec4ac64aa461699a353cdc701dcc020ba30
zakzakst <e1433104@gmail.com>
2022-04-20 09:48:50 +0900

    Async/Await

--- __tests__/practice.test.js
+++ __tests__/practice.test.js
@@ -88,14 +88,43 @@
 // });
 
 function fetchData() {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve('peanut butter');
+      // resolve('peanut butter');
+      reject('error');
     }, 1000);
   });
 }
-test('the data is peanut butter', () => {
-  return fetchData().then(data => {
-    expect(data).toBe('peanut butter');
-  });
+// test('the data is peanut butter', () => {
+//   return fetchData().then(data => {
+//     expect(data).toBe('peanut butter');
+//   });
+// });
+
+// test('the data is peanut butter', async () => {
+//   const data = await fetchData();
+//   expect(data).toBe('peanut butter');
+// });
+
+// test('the fetch fails with an error', async () => {
+//   expect.assertions(1);
+//   try {
+//     await fetchData();
+//   } catch (e) {
+//     expect(e).toMatch('error');
+//   }
+// });
+
+// test('the data is peanut butter', async () => {
+//   await expect(fetchData()).resolves.toBe('peanut butter');
+// });
+
+// test('the fetch fails with an error', async () => {
+//   await expect(fetchData()).rejects.toMatch('error');
+// });
+
+// promiseがrejectされることを期待するケースでは .catch メソッドを使用
+test('the fetch fails with an error', () => {
+  expect.assertions(1);
+  return fetchData().catch(e => expect(e).toMatch('error'));
 });
